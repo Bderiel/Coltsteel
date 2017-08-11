2fc473fa0b5a73c3aa4136e1163e8e0aa946db4b
Brian4292 <bderiel@gmail.com>
2017-08-10 23:55:52 -0400

    Bootrap and JS section progress

---
+++ JS/Js Notes.js
@@ -0,0 +1,73 @@
+//1. 
+console.log((100 % 3) === 1)
+//2.
+console.log(('blah' + 'blah')[6] === 'a')
+//3.
+console.log(('hello'.length % 'hi\\'.length) === 2)
+
+
+function loop1() {
+    let i = -10
+    while (i + 1 < 19) {
+        console.log(i)
+        i++
+    }
+}
+
+function loop2() {
+    let i = 10;
+    while (i + 2 < 40) {
+        console.log(i)
+        i += 2
+    }
+}
+
+function loop3() {
+    let i = 300
+    while (i + 1 < 333) {
+        console.log(i)
+        i++
+    }
+}
+
+function loop4() {
+    let i = 5
+    const arr = []
+    while (i + 1 < 50) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i)
+            arr.push(i)
+        }
+        i++
+    }
+    console.log(arr)
+}
+
+//loop1()
+// loop2()
+// loop3()
+// loop4()
+
+function fLoop1() {
+    for (let i = -10; i <= 19; i++) {
+        console.log(i)
+    }
+}
+
+function fLoop2() {
+    for (let i = 10; i <= 40; i += 2) {
+        console.log(i)
+    }
+}
+
+function fLoop3() {
+    // body...
+}
+
+function fLoop4() {
+    f
+}
+
+
+//fLoop1()
+fLoop2()
